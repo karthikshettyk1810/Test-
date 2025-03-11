5a5b986eef18f2242c15162a986c0f92ebb08a85
Karthik Karthik shetty <karthikshetty1810@gmail.com>
2025-03-11 20:56:52 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import os
+import datetime
+import subprocess
+
+app = Flask(_name_)
+
+@app.route('/htop')
+def htop():
+    username = os.getenv("USER", "codespace-user")
+    ist_time = datetime.datetime.utcnow() + datetime.timedelta(hours=5, minutes=30)
+    try:
+        top_output = subprocess.check_output("top -b -n 1", shell=True).decode("utf-8")
+    except Exception as e:
+        top_output = str(e)
+    response = f"""
+    <h1>System Info</h1>
+    <p><b>Name:</b> Karthik Shetty K</p>
+    <p><b>Username:</b> {username}</p>
+    <p><b>Server Time (IST):</b> {ist_time}</p>
+    <pre>{top_output}</pre>
+    """
+    return response
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
